a9911e14497177726a0433ade70feb3d61169be5
Mohammad Adil <mohammadadil3292@gmail.com>
2024-05-26 12:39:12 +0530

    initial commit

---
+++ client/src/config.jsx
@@ -0,0 +1,7 @@
+// routes
+import { PATH_DASHBOARD } from "./routes/paths";
+
+export const BASE_URL = "http://localhost:3000";
+
+// DEFAULT ROOT PATH
+export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongosanitize = require("express-mongo-sanitize");
+const mongoose = require("mongoose");
+const routes = require("./routes/index");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(mongosanitize());
+app.use(routes);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+const DB = process.env.DBURI.replace("<PASSWORD>", process.env.DBPASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+  
+// Start server
+// app.listen(PORT, () => {
+//     console.log(`App running on PORT ${PORT} ...`);
+// });
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  console.log("UNCAUGHT Exception! Shutting down ...");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  console.log("UNHANDLED REJECTION! Shutting down ...");
+  app.close(() => {
+    process.exit(1);
+  });
+});
